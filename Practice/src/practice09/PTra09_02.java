be842188e731a8ca94b8db1b1b3b5abf1c36490d
TsuchiyaTatsuki <kakakikiup1@gmail.com>
2018-03-22 13:20:48 +0900

    Practice 09_02　まで完了

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -12,9 +12,16 @@ public class PTra09_02 {
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
+		practice09.util.Util.shuffleArray(array);
+
 
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
+		for (String i : array) {
+
+			System.out.println(i);
+		}
+
 
 	}
 }
