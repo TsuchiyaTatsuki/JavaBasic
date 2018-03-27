d56513e166444d05df8bf3dfcd3d1d78d82d69bb
TsuchiyaTatsuki <kakakikiup1@gmail.com>
2018-03-27 11:35:18 +0900

    practice15_02まで完了

--- Practice/src/practice15/common/Room.java
+++ Practice/src/practice15/common/Room.java
@@ -8,7 +8,7 @@ package practice15.common;
 
 import java.util.Date;
 
-public class Room {
+public abstract class Room {
 	/** 教室名 */
 	private String className;
 	/** 担当講師 */
@@ -58,4 +58,8 @@ public class Room {
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
+	public abstract String[] getCurriculum();
+
+
 }
